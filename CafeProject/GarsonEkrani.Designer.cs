ce698b64427fd06e4df83c270349aeb298be4efb
btlkrbc <betulkirbacc@gmail.com>
2015-09-07 13:25:36 +0300

    garson Ekranı

--- CafeProject/GarsonEkrani.Designer.cs
+++ CafeProject/GarsonEkrani.Designer.cs
@@ -29,31 +29,26 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackgroundImage = global::CafeProject.Properties.Resources.images;
-            this.button1.Location = new System.Drawing.Point(509, 46);
+            this.button1.Location = new System.Drawing.Point(572, 9);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(47, 34);
             this.button1.TabIndex = 0;
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(340, 52);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(128, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "ÖdemeBilgileri";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
@@ -67,19 +62,79 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(23, 52);
+            this.dataGridView1.Location = new System.Drawing.Point(37, 46);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(311, 176);
+            this.dataGridView1.Size = new System.Drawing.Size(582, 176);
             this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(541, 250);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(150, 247);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(119, 108);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 247);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Müşteinin Siparişleri";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(469, 250);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Ödeme Türü";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(275, 250);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Hesap Tutarı";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(349, 250);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Visible = false;
             // 
             // GarsonEkrani
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.ClientSize = new System.Drawing.Size(631, 361);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "GarsonEkrani";
             this.Text = "Garson";
@@ -93,8 +148,13 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
--- CafeProject/GarsonEkrani.cs
+++ CafeProject/GarsonEkrani.cs
@@ -1,4 +1,4 @@
-ï»¿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,55 +15,129 @@ namespace CafeProject
     {
         GarsonGuncellemeEkrani gr = new GarsonGuncellemeEkrani();
         DB db = new DB();
+      
+        int gelenID=10;//Garson id 
         public GarsonEkrani()
         {
             InitializeComponent();
         }
 
-     
+        
         private void button1_Click(object sender, EventArgs e)
         {
-            gr.Show(); //Button'a tÄ±kladÄ±ÄŸÄ±mÄ±z zaman garson gÃ¼ncelleme ekranÄ±na geÃ§mesini saÄŸlÄ±yoruz
+            gr.Show(); //Button'a týkladýðýmýz zaman garson güncelleme ekranýna geçmesini saðlýyoruz
 
             this.Hide();
         }
 
+        public void DataGetir()
+        {
+            SqlCommand cm = new SqlCommand("SP_MasaBilgler", db.baglan());
+            cm.CommandType = CommandType.StoredProcedure;
+            cm.Parameters.Add("@id", SqlDbType.Int).Value = gelenID;
+            cm.ExecuteNonQuery();
+
+
+            SqlDataReader rd = cm.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(rd);
+            dataGridView1.DataSource = dt;
+            db.DbKapat();
+        }
         private void GarsonEkrani_Load(object sender, EventArgs e)
         {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            SqlCommand cm = new SqlCommand("SP_Odemetur", db.baglan());
-            bool odemeTuru=true;
-            cm.CommandType = CommandType.StoredProcedure;
-            cm.Parameters.Add("@odemeturu",SqlDbType.Bit).Value=odemeTuru;
-            cm.ExecuteNonQuery();
-            SqlDataAdapter da = new SqlDataAdapter();
-            DataTable dt = new DataTable();
-            da.Fill(dt);
-            dataGridView1.DataSource = dt;
             
-
-            //SqlDataReader rd = db.DataGetir("select odemeturu from siparis ");
-            //rd.Read();
-            //String odemeturu = rd["odemeturu"].ToString();
-
-            //if (odemeturu == "False")
-            //{
-
-            //    MessageBox.Show("kredi kartÄ±");
-
-            //}
-            //else
-            //{
-
-            //    MessageBox.Show("nakit");
-            //}
-
-        db.DbKapat();
-
+            DataGetir();
+            renklendir();
         }
+
+        public void renklendir() 
+        { 
+        
+         try
+            {
+ 
+                for (int i = 0; i < dataGridView1.Rows.Count-1; i++)
+                {
+                    Application.DoEvents();
+ 
+                    DataGridViewCellStyle rowColor = new DataGridViewCellStyle();
+ 
+                    //durum sutunundaki degere göre satir rengi degistiriyoruz.
+                    if (Convert.ToInt32(dataGridView1.Rows[i].Cells[3].Value)==0)//Masa Dolu
+                    {
+                        
+                        // Masa dolu olanlar OrangeRed rengini veriyoruz.
+                        rowColor.BackColor = Color.Pink;
+                        //yazi rengi beyaz oluyor.
+                        rowColor.ForeColor = Color.White;
+                    }
+                    else if (Convert.ToInt32(dataGridView1.Rows[i].Cells[3].Value)==1)//Ödeme Yapýlmýs masa avaliable
+                    {
+                        rowColor.BackColor = Color.Blue;
+                        rowColor.ForeColor = Color.White;
+                    }
+                    else if (Convert.ToInt32(dataGridView1.Rows[i].Cells[3].Value)== 2)//Aþcýya sipariþ verildi
+                    {
+                        rowColor.BackColor = Color.OrangeRed;
+                        rowColor.ForeColor = Color.White;
+                    }
+ 
+                    //satir rengini degistiriyoruz.
+
+                dataGridView1.Rows[i].DefaultCellStyle = rowColor;
+                }
+ 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata:" + ex);
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+
+            //    ToolTip Aciklama = new ToolTip();
+            //    var cell = dataGridView1.CurrentCell;
+            //    var cellDisplayRect = dataGridView1.GetCellDisplayRectangle(e.ColumnIndex, e.RowIndex, false);
+            //    Aciklama.ToolTipTitle = "Dikkat!";
+            //    Aciklama.ToolTipIcon = ToolTipIcon.Warning;
+            //    Aciklama.IsBalloon = true;
+
+            //    dataGridView1.ShowCellToolTips = false;
+
+
+
+            String refKodu = Convert.ToString(dataGridView1.CurrentRow.Cells["refKodu"].Value);
+            if (refKodu!="")
+            {
+                SqlCommand cmd = new SqlCommand("SP_SiparisBilgileri", db.baglan());
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@refKodu", SqlDbType.Int).Value = refKodu;
+                cmd.ExecuteNonQuery();
+                SqlDataReader rdd = cmd.ExecuteReader();
+                if (rdd.Read())
+                {
+                    textBox1.Text = rdd["adi"].ToString();
+                    label6.Visible = true;
+                    label6.Text = rdd["toplamTutar"].ToString();
+                }
+                db.DbKapat();
+                SqlCommand cm = new SqlCommand("SP_OdemeTur", db.baglan());
+                cm.CommandType = CommandType.StoredProcedure;
+                cm.Parameters.Add("@refKodu", SqlDbType.Int).Value = refKodu;
+                cm.ExecuteNonQuery();
+                SqlDataReader rd = cm.ExecuteReader();                
+                
+              }
+        
+
+           
+        }
+
+
+        
     }
 }
