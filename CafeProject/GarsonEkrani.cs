17278fe2323c76e0fc821a59ef3eb72554e48f39
btlkrbc <betulkirbacc@gmail.com>
2015-09-09 11:54:16 +0300

    Garson Ekranı Thread
    Garson Ekranı Thread

--- CafeProject/GarsonEkrani.cs
+++ CafeProject/GarsonEkrani.cs
@@ -9,13 +9,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Threading;
+
 namespace CafeProject
 {
     public partial class GarsonEkrani : Form
     {
         GarsonGuncellemeEkrani gr = new GarsonGuncellemeEkrani();
         DbProcess db = new DbProcess();
-      
+
         int gelenID;//Garson id 
         public GarsonEkrani()
         {
@@ -27,7 +29,30 @@ namespace CafeProject
             gelenID = id;
         }
 
-        
+
+        private void ThreadTask()
+        {
+         
+                    while (true)
+                    {
+                        this.Invoke(new MethodInvoker(delegate()
+                         {
+                             DataGetir();
+
+                         }));
+                        Thread.Sleep(5000);
+               }
+        }
+        private void GarsonEkrani_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            Thread trd = new Thread(new ThreadStart(ThreadTask));
+            trd.Start();
+
+            DataGetir();
+            renklendir();
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             db.dbClose();
@@ -45,57 +70,52 @@ namespace CafeProject
             db.dbConnect();
             cm.ExecuteNonQuery();
 
-
             SqlDataReader rd = cm.ExecuteReader();
             DataTable dt = new DataTable();
             dt.Load(rd);
             dataGridView1.DataSource = dt;
             db.dbClose();
-        }
-        private void GarsonEkrani_Load(object sender, EventArgs e)
-        {
-            
-            DataGetir();
-            renklendir();
+
+            Thread.Sleep(1000);
         }
 
-        public void renklendir() 
-        { 
-        
-         try
+        public void renklendir()
+        {
+
+            try
             {
- 
-                for (int i = 0; i < dataGridView1.Rows.Count-1; i++)
+
+                for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
                 {
                     Application.DoEvents();
- 
+
                     DataGridViewCellStyle rowColor = new DataGridViewCellStyle();
- 
+
                     //durum sutunundaki degere göre satir rengi degistiriyoruz.
-                    if (Convert.ToInt32(dataGridView1.Rows[i].Cells[3].Value)==0)//Masa Dolu
+                    if (Convert.ToInt32(dataGridView1.Rows[i].Cells[3].Value) == 0)//Masa Dolu
                     {
-                        
+
                         // Masa dolu olanlar OrangeRed rengini veriyoruz.
                         rowColor.BackColor = Color.Pink;
                         //yazi rengi beyaz oluyor.
                         rowColor.ForeColor = Color.White;
                     }
-                    else if (Convert.ToInt32(dataGridView1.Rows[i].Cells[3].Value)==1)//Ödeme Yapýlmýs masa avaliable
+                    else if (Convert.ToInt32(dataGridView1.Rows[i].Cells[3].Value) == 1)//Ödeme Yapýlmýs masa avaliable
                     {
                         rowColor.BackColor = Color.Blue;
                         rowColor.ForeColor = Color.White;
                     }
-                    else if (Convert.ToInt32(dataGridView1.Rows[i].Cells[3].Value)== 2)//Aþcýya sipariþ verildi
+                    else if (Convert.ToInt32(dataGridView1.Rows[i].Cells[3].Value) == 2)//Aþcýya sipariþ verildi
                     {
                         rowColor.BackColor = Color.OrangeRed;
                         rowColor.ForeColor = Color.White;
                     }
- 
+
                     //satir rengini degistiriyoruz.
 
-                dataGridView1.Rows[i].DefaultCellStyle = rowColor;
+                    dataGridView1.Rows[i].DefaultCellStyle = rowColor;
                 }
- 
+
             }
             catch (Exception ex)
             {
@@ -108,7 +128,7 @@ namespace CafeProject
 
 
             String refKodu = Convert.ToString(dataGridView1.CurrentRow.Cells["refKodu"].Value);
-            if (refKodu!="")
+            if (refKodu != "")
             {
                 SqlCommand cmd = new SqlCommand("SP_SiparisBilgileri", db.dbConnect());
                 cmd.CommandType = CommandType.StoredProcedure;
@@ -129,31 +149,31 @@ namespace CafeProject
                 SqlDataReader rd = cm.ExecuteReader();
 
                 if (rd.Read())
-                { 
+                {
                     String odeme = rd["odemeTur"].ToString();
-                   if (odeme.Equals(""))
-                              {
-                label2.Visible = true;
-                label2.Text = "KREDÝ KARTI";
-                
-                             }
+                    if (odeme.Equals(""))
+                    {
+                        label2.Visible = true;
+                        label2.Text = "KREDÝ KARTI";
+
+                    }
                     else
-                         {
-                label2.Visible = true;
-                label2.Text = "NAKÝT";
-                          }
+                    {
+                        label2.Visible = true;
+                        label2.Text = "NAKÝT";
+                    }
 
-            db.dbClose();
+                    db.dbClose();
+                }
+
+
             }
-                
-                
-              }
-        
 
-           
+
+
         }
 
 
-        
+
     }
 }
